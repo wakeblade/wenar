98edfcd52c5c557d8f1e7e5c4c3d53752f39be5a
wakeblade <2390245@qq.com>
2019-11-03 11:44:27 +0800

    改端口

--- runserver.py
+++ runserver.py
@@ -8,7 +8,7 @@ from demo import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT,debug=True)
